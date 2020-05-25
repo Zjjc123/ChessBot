89796b95c00861a5ec7974eea46ade4a333a1815
Jason Zhang <zjjc123@hotmail.com>
2020-05-24 17:54:48 -0700

    Getting Board Dimensions

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from selenium import webdriver 
+from stockfish import Stockfish
+import re
 
 browser = webdriver.Chrome("chromedriver.exe")
-
 browser.get("https://www.chess.com/play/computer")
+
+board = browser.find_element_by_xpath('//*[@id="chessboard_boardarea"]')
+
+board_styles = board.get_attribute('style')
+board_width = board_styles.split('width: ')[1].split('px')[0]
+board_height = board_styles.split('height: ')[1].split('px')[0]
+
+print("Board Dimension: " + board_width + "x" + board_height)
+
